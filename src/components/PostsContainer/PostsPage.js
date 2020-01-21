7809ff75ce1d773bc3dc39efc27075a8b961069a
Alex Miller <alexmillerprojects@gmail.com>
2020-01-21 15:05:46 -0800

    finished project

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,13 +1,18 @@
 //Complete the necessary code in this file
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
+import LikeSection from "./LikeSection";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = props => {
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here */}
+      {props.postData.map(p => (
+        <Post key={p.imageUrl}
+        postData={p}/>
+      ))}
+      
     </div>
   );
 };
